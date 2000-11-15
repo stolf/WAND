764fd6c8ee6a83f21bf3c9ffa6615bf64f5f6533
isomer <isomer>
2000-11-15 10:11:07 +0000

    * Added missing mainloop

---
+++ Ethernet-Over-UDP/src/mainloop.cc
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <map>
+#include "mainloop.h"
+
+typedef map<int,callback_t> fd2callback_t;
+
+static fd2callback_t fd2callback;
+
+static fd_set rfd;
+static int highestfd = 0;
+
+void addRead(int fd,callback_t callback)
+{
+	FD_SET(fd,&rfd);
+	fd2callback[fd]=callback;
+	if (highestfd<fd)
+		highestfd=fd;
+}
+
+void remRead(int fd)
+{
+	FD_CLR(fd,&rfd);
+}
+
+void mainloop(void)
+{
+	fd_set rfd2;
+
+	while(1) {
+		rfd2=rfd;
+		select(highestfd+1,&rfd2,NULL,NULL,NULL);
+		for (fd2callback_t::const_iterator i=fd2callback.begin();
+		     i!=fd2callback.end();
+		     i++)
+			if (FD_ISSET(i->first,&rfd2))
+				i->second(i->first);
+	}
+}
