a5babc655ab793039ebbf08be5052c69c14af999
gsharp <gsharp>
2001-08-12 07:27:47 +0000

    Added daemonise to Etud (forks into the background) Squashed bugs in ethertap driver

--- Ethernet-Over-UDP/drivers/ethertap.c
+++ Ethernet-Over-UDP/drivers/ethertap.c
@@ -1,5 +1,5 @@
 /* Wand Project - Ethernet Over UDP
- * $Id: ethertap.c,v 1.5 2001/08/12 06:00:27 gsharp Exp $
+ * $Id: ethertap.c,v 1.6 2001/08/12 07:27:47 gsharp Exp $
  * Licensed under the GPL, see file COPYING in the top level for more
  * details.
  */
@@ -35,76 +35,23 @@
 static int fd = -1;
 static int tapdevno;
 
-static int ethertap_setup(unsigned long myid) {
-	
-	struct ifreq ifr;
-	int skfd;
+static int ethertap_setup(unsigned long myid) 
+{
 	char tapdevice[16];
-
-	char hwaddr[6] = { 
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-	};
+	printf("ethertap_setup( %lu ) entered...\n", myid);
 
 	fd = 0;	
-	tapdevno = 0;
-
+	tapdevno = 3;
 	
-	memcpy(hwaddr, &(myid)+(6-sizeof(myid)), 4);
-	
-    	/* Open a socket so we can ioctl() */
-        if ((skfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-                perror("socket");
-                exit(1);
-        }
-
-               	
 	while (tapdevno<MAX_ETHERTAP_DEVICES) {
-		snprintf(tapdevice, 255, "/dev/tap%d", tapdevno);
+		snprintf(tapdevice, 16, "/dev/tap%d", tapdevno);
 		fd = open(tapdevice, O_RDWR);					
-
-		if (fd > 1) {
-
-   			/* Get interface flags */
-   			sprintf(ifr.ifr_name, "tap%d", tapdevno);
-
-  	  		if (ioctl(skfd, SIOCGIFFLAGS, &ifr) < 0) {
-   	     			fprintf(stderr, "%s: unknown interface: %s\n", 
-                		ifr.ifr_name, strerror(errno));
-        			return -1;
-    			}
-
-			/* Set the interface to DOWN */
-			ifr.ifr_flags &= ~IFF_UP; /* Interfaces don't like their HWADDR changed on the fly! */
-		   	if (ioctl(skfd, SIOCSIFFLAGS, &ifr) < 0) {
-			       	perror("SIOCSIFFLAGS");
-       	 			return -1;
-    			}
-#if 0			
-			/* Set the HWADDR */
-			ifr.ifr_netmask.sa_family = ARPHRD_ETHER;
-
-			memcpy(ifr.ifr_netmask.sa_data, hwaddr, 6);
-    			if (ioctl(skfd, SIOCSIFHWADDR, &ifr) < 0) {
-            			perror("SIOCSIFHWADDR");
-        			return -1;
-    			}
-#endif    			
-			/* MTU is 576 */
-			ifr.ifr_mtu = 576;
-            		if (ioctl(skfd, SIOCSIFMTU, &ifr) < 0) {
-                            	fprintf(stderr, "SIOCSIFMTU: %s\n", strerror(errno));
-				return -1;
-                        }
-                                                        
-			close (skfd);
-			fprintf(stderr,"ethertap bound to device /dev/tap%i\n",tapdevno);
-			return fd;
-		}			
+		if( fd >= 0 ) {
+			return fd;			
+		}
 		tapdevno++;
 	}
-
-
-	close (skfd);
+	
 	printf("can't open ethertap device, aborting\n");
 	return -1;
 }
@@ -151,7 +98,7 @@ static int ethertap_write(char *frame, int sz)
 
 static struct interface_t ethertap = {
 	"ethertap",
-	"$Id: ethertap.c,v 1.5 2001/08/12 06:00:27 gsharp Exp $",
+	"$Id: ethertap.c,v 1.6 2001/08/12 07:27:47 gsharp Exp $",
 	ethertap_setup,
 	ethertap_down,
 	ethertap_read,
--- Ethernet-Over-UDP/src/Etud.cc
+++ Ethernet-Over-UDP/src/Etud.cc
@@ -1,21 +1,68 @@
 /* Wand Project - Ethernet Over UDP
- * $Id: Etud.cc,v 1.6 2001/08/12 06:00:27 gsharp Exp $
+ * $Id: Etud.cc,v 1.7 2001/08/12 07:27:47 gsharp Exp $
  * Licensed under the GPL, see file COPYING in the top level for more
  * details.
  */
 
 #include <sys/time.h> /* for select */
-#include <sys/types.h> /* for select */
+#include <sys/types.h> /* for select, umask and open */
+#include <sys/stat.h> /* for umask and open */
 #include <net/ethernet.h> /* for ether_header and ETH_ALEN */
 #include <iostream.h> /* for cout */
 #include <dlfcn.h> /* for dlopen */
 #include <unistd.h> /* for select */
+#include <fcntl.h> /* for open */
 
 #include "list.h"
 #include "driver.h"
 #include "udp.h"
 #include "ui.h"
 #include "mainloop.h"
+
+void put_pid(void)
+{
+	char buf[512];
+	int fd=creat("/var/run/Etud.pid",0660);
+	if (fd<0)
+		return;
+	sprintf(buf,"%i\n",getpid());
+	if (write(fd,buf,strlen(buf)) != (signed)strlen(buf)) {
+		close(fd);
+		return;
+	}
+	close(fd);
+}
+
+void daemonise( void ) 
+{
+	switch (fork()) {
+	case 0:
+		break;
+	case -1:
+		perror("fork");
+		exit(1);
+	default:
+		_exit(0);
+	}
+	setsid();
+	switch (fork()) {
+       	case 0:
+       		break;
+       	case -1:
+       		perror("fork2");
+		exit(1);
+	default:
+		_exit(0);
+	}
+	chdir("/");
+	umask(0155);
+	close(0);
+	close(1);
+	close(2);
+	open("/dev/null",O_RDONLY);
+	open("/dev/console",O_WRONLY);
+	open("/dev/console",O_WRONLY);
+}	
 
 int load_module(char *s)
 {
@@ -57,5 +104,12 @@ int main(int arvc,char **argv)
 	cout << "Etud started" << endl;
 	cout << "Using interface driver: " << interface->name << endl;
 	cout << " version: " << interface->version << endl;
+
+	/* Lets go to the background */
+	cout << "Attempting to Daemonise..." << endl;
+	daemonise();
+	put_pid();
+	cout << "Returned from Daemonise sucessfully." << endl;
+
 	mainloop();
 }
