38435246f6c387479dca256399efa95339c8302d
isomer <isomer>
2002-04-17 10:08:08 +0000

    Adding new updated files for configuration file handling

---
+++ lib/config_internal.h
@@ -0,0 +1,6 @@
+#ifndef config_internal
+#define config_internal
+
+int set_config_int(char *key,int value);
+
+#endif
---
+++ lib/libconfig.c
@@ -0,0 +1,40 @@
+#include "config_internal.h"
+#include <config.h>
+#include <stdio.h>
+
+extern FILE *yyin;
+
+static config_t *config_table;
+
+int set_config_int(char *option,int value)
+{
+  config_t *item;
+  printf("Setting %s to %i\n",option,value);
+  for(item=config_table;item->key;item++) {
+    if (!strcmp(item->key,option)) {
+      if ((item->type&TYPE_MASK) == TYPE_INT) {
+      	*((int *)item->value)=value;
+	return 0;
+      }
+      else {
+	printf("%s does not take an int\n",option);
+	/* XXX: Incorrect type */
+      }
+    }
+  }
+  printf("Unknown configuration option %s\n",option);
+  /* XXX: Not found */
+  return 1; 
+}
+
+int parse_config(config_t *config,char *filename)
+{
+  yyin = fopen(filename,"r");
+  config_table=config;
+  if (!yyin) {
+    return 1;
+  }
+  yyparse();
+  fclose(yyin);
+  return 0;
+}
