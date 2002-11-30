6cc1a43f01f1589a9cd44444cb6556aceb60832e
mattgbrown <mattgbrown>
2002-11-30 06:10:26 +0000

    Header file

---
+++ Ethernet-Over-UDP/include/etud.h
@@ -0,0 +1,12 @@
+/* Wand Project - Ethernet Over UDP
+ * $Id: 
+ * Licensed under the GPL, see file COPYING in the top level for more
+ * details.
+ */
+
+#ifndef ETUD_H
+#define ETUD_H
+
+extern char *macaddr;
+
+#endif
